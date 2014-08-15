2a2f002fc94641dfdb72e47cffe8f1488fcef9da
Jeffrey Rothschild <jeffrey.rothschild@gmail.com>
2014-08-14 21:27:26 -0500

    Added anular functionality for live posting of comments

---
+++ app/assets/javascripts/chats_ctrl.js
@@ -0,0 +1,20 @@
+(function () {
+  "use strict";
+
+  angular.module("app").controller("chatsCtrl", function ($scope, $http) {
+    
+      $scope.init = function(url){
+        $scope.item = {url: url};
+      }
+
+    $scope.chat = function(message, url) {
+      $http.post('/comments', {'message' : message, 'url' : $scope.item.url}).then(function(response) {
+     
+        }, function(error) {
+        
+        console.log(error);
+      });
+    }
+    
+  }); 
+})();
